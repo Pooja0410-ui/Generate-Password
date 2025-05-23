390d7508197b12c7558c2e7e7d99b0305b1bf4f9
Pooja <poojapatil4009@gmail.com>
2025-05-23 03:59:31 -0700

    removed unused symbols for password

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const length = 12;
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const number = "0123456789";
-const symbol = "@#$%^&*()_+~|}{[]></-=";
+const symbol = "@#$%^&*()_+~|}{[]-=";
 
 const allchars = uppercase + lowercase + number + symbol;
 
